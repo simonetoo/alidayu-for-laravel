f4ba5d48b32b58485ed15f109a7a1612b9398fbc
vicens <qianqian12>
2018-01-08 20:47:11 +0800

    first commit

--- src/Providers/AlidayuServiceProvider.php
+++ src/Providers/AlidayuServiceProvider.php
@@ -7,34 +7,13 @@ use Vicens\Alidayu\Alidayu;
 
 class AlidayuServiceProvider extends ServiceProvider
 {
-    /**
-     * 按需加载
-     * @var bool
-     */
-    protected $defer = true;
 
-    /**
-     * 注册服务
-     */
-    public function register()
+    public function boot()
     {
-        //注册到容器中
-        $this->app->singleton('alidayu', function () {
 
-            $config = $this->app->make('config')->get('services.alidayu', array());
+        // 启动时设置配置
+        $config = $this->app->make('config')->get('services.alidayu', array());
 
-            return Alidayu::setConfig($config);
-        });
+        return Alidayu::setConfig($config);
     }
-
-    /**
-     * 延迟加载的服务
-     *
-     * @return array
-     */
-    public function provides()
-    {
-        return ['alidayu'];
-    }
-
 }
